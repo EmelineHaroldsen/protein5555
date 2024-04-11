684f0dc514018d50cff9cdd3d41e5bd17c578bac
abbyfunk <abby.funk@usu.edu>
2024-04-11 11:07:20 -0600

    checked filtering with kinase

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -5,10 +5,13 @@ huo <- read.pdb("data/Pol/1huo.pdb")
 sl0 <- read.pdb("data/Pol/1sl0.pdb")
 bqr <- read.pdb("data/Pol/2bqr.pdb")
 
-# Filter 'atom' to only include entries where the atom type is 'SEQRES'
+fpu <- read.pdb("data/Kinase/1fpu.pdb")
+qgy <- read.pdb("data/Kinase/8qgy.pdb")
+r7g <- read.pdb("data/Kinase/8r7g.pdb")
+
+# Filter POL to find chain A
 seq <- huo$seqres
 filtered_seq <- seq[names(seq) == "A"]
-
 
 seq <- sl0$seqres
 filtered_seq <- seq[names(seq) == "A"]
@@ -16,3 +19,11 @@ filtered_seq <- seq[names(seq) == "A"]
 seq <- bqr$seqres
 filtered_seq <- seq[names(seq) == "A"]
 
+seq <- fpu$seqres
+filtered_seq <- seq[names(seq) == "A"]
+
+seq <- qgy$seqres
+filtered_seq <- seq[names(seq) == "A"]
+
+seq <- r7g$seqres
+filtered_seq <- seq[names(seq) == "A"]
