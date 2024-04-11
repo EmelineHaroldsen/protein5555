fdb864e8dd1e99b92de178416df366a090dcc820
abbyfunk <abby.funk@usu.edu>
2024-04-11 11:45:56 -0600

    saved data files

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -1,29 +1,40 @@
 #clean code here
 library(bio3d)
 library(tidyverse)
-huo <- read.pdb("data/Pol/1huo.pdb")
-sl0 <- read.pdb("data/Pol/1sl0.pdb")
-bqr <- read.pdb("data/Pol/2bqr.pdb")
+huo1 <- read.pdb("data/Pol/1huo.pdb")
+sl01 <- read.pdb("data/Pol/1sl0.pdb")
+bqr1 <- read.pdb("data/Pol/2bqr.pdb")
 
-fpu <- read.pdb("data/Kinase/1fpu.pdb")
-qgy <- read.pdb("data/Kinase/8qgy.pdb")
-r7g <- read.pdb("data/Kinase/8r7g.pdb")
+fpu1 <- read.pdb("data/Kinase/1fpu.pdb")
+qgy1 <- read.pdb("data/Kinase/8qgy.pdb")
+r7g1 <- read.pdb("data/Kinase/8r7g.pdb")
+
+#overall generic function
+cleanser <- function(pdb_location){
+  file <- bio3d::read.pdb(pdb_location)
+  seq <- file$seqres
+  filt_seq <- seq[names(seq) == "A"]
+  filt_seq <- as.vector(filt_seq)
+}
+
 
 # Filter POL to find chain A
-seq <- huo$seqres
-filtered_seq <- seq[names(seq) == "A"]
+huo <- huo1$seqres
+filt_huo <- huo[names(huo) == "A"]
 
-seq <- sl0$seqres
-filtered_seq <- seq[names(seq) == "A"]
+sl0 <- sl01$seqres
+filt_sl0 <- sl0[names(sl0) == "A"]
 
-seq <- bqr$seqres
-filtered_seq <- seq[names(seq) == "A"]
+bqr <- bqr1$seqres
+filt_bqr <- bqr[names(bqr) == "A"]
 
-seq <- fpu$seqres
-filtered_seq <- seq[names(seq) == "A"]
+#filter Kinase
+fpu <- fpu1$seqres
+filt_fpu <- fpu[names(fpu) == "A"]
 
-seq <- qgy$seqres
-filtered_seq <- seq[names(seq) == "A"]
+qgy <- qgy1$seqres
+filt_qgy <- qgy[names(qgy) == "A"]
 
-seq <- r7g$seqres
-filtered_seq <- seq[names(seq) == "A"]
+r7g <- r7g1$seqres
+filt_r7g <- r7g[names(r7g) == "A"]
+
