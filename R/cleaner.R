cdcdea999f3be01d8dfe291b49106fbf71fa0f92
abbyfunk <abby.funk@usu.edu>
2024-04-23 12:30:02 -0600

    should be running clean!

--- R/cleaner.R
+++ R/cleaner.R
@@ -21,18 +21,18 @@ cleaner <- function(pdb_location) {
   }
 
   # Create a data frame with the names and values
-  df <- data.frame(string = names(filtered), value = as.character(filtered))
+  df <- data.frame(pro_seq = names(filtered), value = as.character(filtered))
 
   # Check if there are any names
-  if (is.null(df$string)) {
+  if (is.null(df$pro_seq)) {
     return("No names found.")
   }
-
   # Filter for A and B separately
   counts <- df |>
-    dplyr::filter(string %in% c("A", "B")) |>
-    dplyr::group_by(string) |>
+    dplyr::filter(.data$pro_seq %in% c("A", "B")) |>
+    dplyr::group_by(.data$pro_seq) |>
     dplyr::summarize(count = dplyr::n())
+
 
   # Check if counts for both A and B exist
   if (nrow(counts) == 2) {
@@ -40,7 +40,7 @@ cleaner <- function(pdb_location) {
     class <- ifelse(counts$count[1] >= counts$count[2], "A", "B")
 
     # Get the filtered sequence for the chosen class
-    filt_seq <- filtered[df$string == class]
+    filt_seq <- filtered[df$pro_seq == class]
     return(filt_seq)
   } else {
     # Handle case where counts are missing
