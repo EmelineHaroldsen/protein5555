3a0bdad8f77885def00403311d0bffa7f2bd4990
abbyfunk <abby.funk@usu.edu>
2024-04-23 10:57:23 -0600

    lintr checks

--- R/summary_function.R
+++ R/summary_function.R
@@ -8,7 +8,10 @@ summary.protein5555 <- function(object, ...) {
   nonpolar <- count_label_occurrences(df, "type", "nonpolar")
   count <- c(pos, neg, pos + neg, polar, nonpolar, len)
   freq <- round(count / len, 3)
-  charge <- c("positive", "negative", "total_charge", "polar", "nonpolar", "length")
+  charge <- c(
+    "positive", "negative", "total_charge",
+    "polar", "nonpolar", "length"
+  )
   new_df <- data.frame(charge = charge, count = count, frequency = freq)
   print(paste("Family:", attr(object, "family")))
   print(new_df)
@@ -16,32 +19,37 @@ summary.protein5555 <- function(object, ...) {
 
 #' @export
 summary.protein5555_list <- function(object, ...) {
-  uFam <- unique_families(object)
+  u_fam <- unique_families(object)
   print("Families: ")
-  print(uFam)
-  get_full_count(object, uFam)
+  print(u_fam)
+  get_full_count(object, u_fam)
 }
 
-get_full_count <- function(object, uFam) {
+get_full_count <- function(object, u_fam) {
   prot <- 0
   pos <- 0
   neg <- 0
   polar <- 0
-  nonPol <- 0
+  non_pol <- 0
   total <- 0
-  for (l in 1:length(object)) {
+  for (l in seq_along(object)) {
     prot <- prot + 1
     df <- classify(unlist(object[l]))
     pos <- pos + count_label_occurrences(df, "type", "positive")
     neg <- neg + count_label_occurrences(df, "type", "negative")
-    polar <- polar + count_label_occurrences(df, "type", "polar") + count_label_occurrences(df, "type", "negative") + count_label_occurrences(df, "type", "positive")
-    nonPol <- nonPol + count_label_occurrences(df, "type", "nonpolar")
+    polar <- polar + count_label_occurrences(df, "type", "polar") +
+      count_label_occurrences(df, "type", "negative") +
+      count_label_occurrences(df, "type", "positive")
+    non_pol <- non_pol + count_label_occurrences(df, "type", "nonpolar")
     total <- total + length(df$protein)
   }
 
-  count <- c(pos, neg, pos + neg, polar, nonPol, total)
+  count <- c(pos, neg, pos + neg, polar, non_pol, total)
   freq <- round(count / total, 3)
-  charge <- c("positive", "negative", "total_charge", "polar", "nonpolar", "length")
-  allDf <- data.frame(charge = charge, count = count, frequency = freq)
-  print(allDf)
+  charge <- c(
+    "positive", "negative", "total_charge",
+    "polar", "nonpolar", "length"
+  )
+  all_df <- data.frame(charge = charge, count = count, frequency = freq)
+  print(all_df)
 }
